ab7806be407f48db09da547380a5b004ca264aaf
Immanuel Pandiangan <nuelpan@gmail.com>
2019-09-24 09:42:09 +0700

    add exercise-3.js

---
+++ exercise-3.js
@@ -0,0 +1,25 @@
+var nama = "Rund";
+var peran = "Ksatria";
+
+if (nama !== "") {
+  if (peran !== "") {
+    console.log("Selamat datang di Dunia Proxytia, " + nama);
+    if (peran === "Ksatria") {
+      console.log(
+        "Halo " + peran + " " + nama + ", kamu dapat menyerang dengan senjatamu"
+      );
+    } else if (peran === "Tabib") {
+      console.log(
+        "Halo " + peran + " " + nama + ", kamu akan membantu temanmu yang terluka"
+      );
+    } else if (peran === " Penyihir") {
+      console.log(
+        "Halo " + peran + " " + nama + ", ciptakan keajaiban yang membantu kemenanganmu!"
+      );
+    }
+  } else {
+    console.log("Halo " + nama + ", Pilih peranmu untuk memulai game!");
+  }
+} else {
+  console.log("Nama harus diisi!");
+}
