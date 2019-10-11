1835968c047339ab5d9eeb8d0f32d85c46c18ef3
Immanuel Pandiangan <nuelpan@gmail.com>
2019-10-11 13:16:45 +0700

    update operator

--- exercise-10.js
+++ exercise-10.js
@@ -3,7 +3,7 @@ function bandingkanAngka(angka1, angka2) {
         return true;
     } else if (angka2 < angka1) {
         return false;
-    } else if (angka2 = angka1) {
+    } else if (angka2 === angka1) {
         return -1;
     }
 }
